7b90bfd654689ae449e6fce0a45170a3977d479f
Kanu <kanusobti@gmail.com>
2020-02-04 01:15:38 -0500

    deploying City Bikes Build

---
+++ precache-manifest.a18318f2035366ad8e25376ea04444f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4452dacaf3e74b5df067a9d5d2b93f1c",
+    "url": "/kanusobti/CityBikes/index.html"
+  },
+  {
+    "revision": "1c8128af4bc602702cd8",
+    "url": "/kanusobti/CityBikes/static/css/main.b20de872.chunk.css"
+  },
+  {
+    "revision": "477efe04ac6fad3db564",
+    "url": "/kanusobti/CityBikes/static/js/2.a099788b.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/kanusobti/CityBikes/static/js/2.a099788b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c8128af4bc602702cd8",
+    "url": "/kanusobti/CityBikes/static/js/main.7b4a4d8f.chunk.js"
+  },
+  {
+    "revision": "07567f2c23efadc1935a",
+    "url": "/kanusobti/CityBikes/static/js/runtime-main.748cff2e.js"
+  }
+]);
